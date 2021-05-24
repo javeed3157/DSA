43403750e3bc3c90d7ac9193fd331882f71df50e
Javeed Suhail <javeed3157@gmail.com>
2021-05-24 11:31:27 +0530

    Added insertion sort

--- Sorting/Insertion.java
+++ Sorting/Insertion.java
@@ -4,13 +4,13 @@ public class Insertion {
     public static int[] inser(int a[]){
         int n=a.length;
         for(int i=1;i<n;i++){
-            int temp=a[i];
+            int curr=a[i];
             int j=i-1;
-            while(j>=0 && a[j]>temp){
+            while(j>=0 && a[j]>curr){
                 a[j+1]=a[j];
                 j--;
             }
-            a[j+1]=temp;
+            a[j+1]=curr;
         }
         return a;
     }
