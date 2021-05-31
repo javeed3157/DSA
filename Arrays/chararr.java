16ae4fe8aab4c4113877929b12e39717b79287ab
Javeed Suhail <javeed3157@gmail.com>
2021-05-31 08:21:43 +0530

    Inputting character array

---
+++ Arrays/chararr.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class chararr{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        char arr[]=new char[5];
+        for(int i=0;i<5;i++){
+            arr[i]=sc.next().charAt(0);
+        }
+        // for(int i=0;i<arr.length;i++){
+        //     System.out.println(arr[i]);
+        // }
+        for(char i:arr){
+            System.out.println(i);
+        }
+    }
+}
