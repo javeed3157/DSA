1f8c7e199b2cba150ff5a9dcf65fd781f42c6d26
Javeed Suhail <javeed3157@gmail.com>
2021-12-02 22:28:52 +0530

    added basic sort using recursion

---
+++ Recursion/selection.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+public class selection {
+    static void select(int a[],int r,int c,int m){
+        if(r==0){
+            return;
+        }
+        if(c<r){
+            if(a[c]>a[m]){
+                select(a, r,c+1,c);
+            }
+            else{
+                select(a, r,c+1,m);
+            }
+        }
+        else{
+            int temp=a[m];
+            a[m]=a[r-1];
+            a[r-1]=temp;
+            select(a, r-1,0,0);
+        }
+    }
+    public static void main(String[] args) {
+        int arr[]={5,1,3,4,2};
+        select(arr,arr.length,0,0);
+        System.out.println(Arrays.toString(arr));
+    }
+}
