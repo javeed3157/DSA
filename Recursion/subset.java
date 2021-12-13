ae0e825b1f1b98b74762e0f153c3b9ddbc4faae7
Javeed Suhail <javeed3157@gmail.com>
2021-12-13 22:23:58 +0530

    subset using recursion

---
+++ Recursion/subset.java
@@ -0,0 +1,14 @@
+public class subset{
+    public static void main(String[] args) {
+        sub("", "abc");
+    }
+    static void sub(String p,String up){
+        if(up.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+        char ch=up.charAt(0);
+        sub(p+ch, up.substring(1));
+        sub(p, up.substring(1));
+    }
+}
--- Recursion/sunseq.java
+++
@@ -1,6 +0,0 @@
-public class sunseq{
-    public static void main(String[] args) {
-        int a[]={1,2,3,4,5};
-        
-    }
-}
