4e39234df05c055c62dc18040efacc6309c0d61d
Javeed Suhail <javeed3157@gmail.com>
2021-12-15 22:10:09 +0530

    added basics of recur

--- Recursion/subset.java
+++ Recursion/subset.java
@@ -2,8 +2,9 @@ import java.util.ArrayList;
 
 public class subset{
     public static void main(String[] args) {
-        // sub("", "abc");
+        sub("", "abc");
         System.out.println(subs("", "abc"));
+        subacii("", "abc");
     }
     static void sub(String p,String up){
         if(up.isEmpty()){
@@ -13,6 +14,16 @@ public class subset{
         char ch=up.charAt(0);
         sub(p+ch, up.substring(1));
         sub(p, up.substring(1));
+    }
+    static void subacii(String p,String up){
+        if(up.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+        char ch=up.charAt(0);
+        subacii(p+ch, up.substring(1));
+        subacii(p, up.substring(1));
+        subacii(p+(ch+0),up.substring(1));
     }
     static ArrayList<String> subs(String p,String up){
         if(up.isEmpty()){
