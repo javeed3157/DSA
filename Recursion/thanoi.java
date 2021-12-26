4fd517cc5764cadd23a8a25889a82602fb047532
Javeed Suhail <javeed3157@gmail.com>
2021-12-26 19:37:25 +0530

    added towers of hanoi

--- Recursion/thanoi.java
+++ Recursion/thanoi.java
@@ -1,12 +1,13 @@
+// Time:O(2^n-1) ~ O(2^n)
 public class thanoi {
     public static void towerofhanoi(int n,String s,String h,String d){
         if(n==1){
             System.out.println("Transfer the disc "+n+" "+ "from" +" "+s+" "+"to"+" "+ d);
             return;
         }
-        towerofhanoi(n-1, s, d, h);
-        System.out.println("Transfer the disc "+n+" "+ "from" +" "+s+" "+"to"+" "+ d);
-        towerofhanoi(n-1, h, s, d);
+        towerofhanoi(n-1, s, d, h);//1st step tranfering n-1 discs from Src to Help using Dest
+        System.out.println("Transfer the disc "+n+" "+ "from" +" "+s+" "+"to"+" "+ d);//nth number disc transfer
+        towerofhanoi(n-1, h, s, d);//2nd step tranfering n-1 discs from Help to Dest using Src
     }
     
     public static void main(String[] args) {
